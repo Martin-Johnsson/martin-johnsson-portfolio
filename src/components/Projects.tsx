bf43163a073a4a7b3f00f2e234cb68a26834fa90
Martin Johnsson <maartinjohnsson@gmail.com>
2023-06-09 16:32:10 +0200

    Fix ternary so it works for both larger and smaller screens

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -39,131 +39,132 @@ const Projects = () => {
   return (
     <ProjectsSection>
       {!isSmallerThanLg && (
-        <>
-          <Wrap
-            justify='center'
-            spacing='10vh'
-            align='center'
-            marginBottom='2vh'
-            marginTop='11vh'
-          >
-            <Box textAlign='center'>
-              <BigHeading>Projects</BigHeading>
-            </Box>
+        <Wrap
+          justify='center'
+          spacing='10vh'
+          align='center'
+          marginBottom='2vh'
+          marginTop='11vh'
+        >
+          <Box textAlign='center'>
+            <BigHeading>Projects</BigHeading>
+          </Box>
 
-            {projects.map((element, index) => (
-              <WrapItem>
-                <Box
-                  boxShadow='0.14vh 0.14vw 1.98vh black '
-                  key={index}
-                  cursor='pointer'
-                  onClick={() => {
-                    handleExpandProject(index);
-                  }}
-                  w='90vw'
-                >
-                  <Flex direction='column' alignItems={'center'}>
-                    <Box alignSelf='center'>
-                      <BodyText>{element.name}</BodyText>
-                      <SmallBodyText>{element.description}</SmallBodyText>
-                      <Flex marginRight='1vw' flexWrap='wrap'>
-                        {element.tags.map((tag) => {
-                          return (
-                            <Box
-                              max-w='5'
-                              border='0.2rem solid #0b3d54'
-                              color='#f5f0f0'
-                              marginLeft='0.6vw'
-                              display='inline-block'
-                              padding='0.2vw'
-                            >
-                              {tag}
-                            </Box>
-                          );
-                        })}
-                      </Flex>
-                    </Box>
-                    <Box
-                      justifyContent='center'
-                      marginBottom='10vh'
-                      marginTop='5vh'
-                    >
-                      <Img
-                        src={element.frontImage}
-                        alt='Screenshot of Insights application'
-                        borderRadius='1.5vw'
-                      />
-                    </Box>
-                  </Flex>
-                </Box>
-              </WrapItem>
-            ))}
-          </Wrap>
-        </>
+          {projects.map((element, index) => (
+            <WrapItem>
+              <Box
+                boxShadow='0.14vh 0.14vw 1.98vh black '
+                key={index}
+                cursor='pointer'
+                onClick={() => {
+                  handleExpandProject(index);
+                }}
+                w='90vw'
+              >
+                <Flex direction='column' alignItems={'center'}>
+                  <Box alignSelf='center'>
+                    <BodyText>{element.name}</BodyText>
+                    <SmallBodyText>{element.description}</SmallBodyText>
+                    <Flex marginRight='1vw' flexWrap='wrap'>
+                      {element.tags.map((tag) => {
+                        return (
+                          <Box
+                            max-w='5'
+                            border='0.2rem solid #0b3d54'
+                            color='#f5f0f0'
+                            marginLeft='0.6vw'
+                            display='inline-block'
+                            padding='0.2vw'
+                          >
+                            {tag}
+                          </Box>
+                        );
+                      })}
+                    </Flex>
+                  </Box>
+                  <Box
+                    justifyContent='center'
+                    marginBottom='10vh'
+                    marginTop='5vh'
+                  >
+                    <Img
+                      src={element.frontImage}
+                      alt='Screenshot of Insights application'
+                      borderRadius='1.5vw'
+                      width=''
+                    />
+                  </Box>
+                </Flex>
+              </Box>
+            </WrapItem>
+          ))}
+        </Wrap>
       )}
-      <Wrap
-        justify='center'
-        spacing='10vh'
-        align='center'
-        marginBottom='2vh'
-        marginTop='11vh'
-      >
-        <Box textAlign='center'>
-          <BigHeading>Projects</BigHeading>
-        </Box>
+      {isSmallerThanLg && (
+        <Wrap
+          justify='center'
+          spacing='10vh'
+          align='center'
+          marginBottom='2vh'
+          marginTop='11vh'
+        >
+          <Box textAlign='center'>
+            <BigHeading>Projects</BigHeading>
+          </Box>
 
-        {projects.map((element, index) => (
-          <WrapItem>
-            <Box
-              boxShadow='0.14vh 0.14vw 1.98vh black '
-              key={index}
-              cursor='pointer'
-              onClick={() => {
-                handleExpandProject(index);
-              }}
-              w='90vw'
-            >
-              <Flex direction='column' alignItems={'center'}>
-                <Box alignSelf='center'>
-                  <SmallScreenBodyText>{element.name}</SmallScreenBodyText>
-                  <SmallScreenSmallBodyText>
-                    {element.description}
-                  </SmallScreenSmallBodyText>
-                  <Flex marginRight='1vw' flexWrap='wrap' marginTop='1.5vh'>
-                    {element.tags.map((tag) => {
-                      return (
-                        <Box
-                          max-w='5'
-                          border='0.2rem solid #0b3d54'
-                          color='#f5f0f0'
-                          marginLeft='2vw'
-                          marginTop='1vh'
-                          display='inline-block'
-                          padding='0.2vw'
-                        >
-                          {tag}
-                        </Box>
-                      );
-                    })}
-                  </Flex>
-                </Box>
-                <Box
-                  justifyContent='center'
-                  marginBottom='10vh'
-                  marginTop='5vh'
-                >
-                  <Img
-                    src={element.frontImage}
-                    alt='Screenshot of Insights application'
-                    borderRadius='1.5vw'
-                    width='70vw'
-                  />
-                </Box>
-              </Flex>
-            </Box>
-          </WrapItem>
-        ))}
-      </Wrap>
+          {projects.map((element, index) => (
+            <WrapItem>
+              <Box
+                boxShadow='0.14vh 0.14vw 1.98vh black '
+                key={index}
+                cursor='pointer'
+                onClick={() => {
+                  handleExpandProject(index);
+                }}
+                w='90vw'
+              >
+                <Flex direction='column' alignItems={'center'}>
+                  <Box alignSelf='center'>
+                    <SmallScreenBodyText>{element.name}</SmallScreenBodyText>
+                    <SmallScreenSmallBodyText>
+                      {element.description}
+                    </SmallScreenSmallBodyText>
+                    <Flex marginRight='1vw' flexWrap='wrap' marginTop='1.5vh'>
+                      {element.tags.map((tag) => {
+                        return (
+                          <Box
+                            max-w='5'
+                            border='0.2rem solid #0b3d54'
+                            color='#f5f0f0'
+                            marginLeft='2vw'
+                            marginTop='1vh'
+                            display='inline-block'
+                            padding='0.2vw'
+                          >
+                            {tag}
+                          </Box>
+                        );
+                      })}
+                    </Flex>
+                  </Box>
+                  <Box
+                    justifyContent='center'
+                    marginBottom='10vh'
+                    marginTop='5vh'
+                  >
+                    <Img
+                      src={element.frontImage}
+                      alt='Screenshot of Insights application'
+                      borderRadius='1.5vw'
+                      width='70vw'
+                    />
+                  </Box>
+                </Flex>
+              </Box>
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
       {projectsState.modalIsOpen && (
         <ProjectsModal
           modalIsOpen={projectsState.modalIsOpen}
