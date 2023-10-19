b443058cbac88761f8c460563a8b859b0814e513
Martin Johnsson <maartinjohnsson@gmail.com>
2023-10-20 00:25:39 +0200

    Add support for absolute paths using alias src in tsconfig.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+    },
+  },
   server: {
     port: 3001,
   },
