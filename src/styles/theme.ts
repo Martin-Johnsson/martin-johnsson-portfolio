8ce56506a7673b2e69804729876f9b7b5b073c09
Martin Johnsson <maartinjohnsson@gmail.com>
2023-10-19 18:36:59 +0200

    Create theme.ts and create global color object

---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  textColor: '#f5f0f0',
+  primaryAccentColor: '#483ab0',
+  secondaryAccentColor: '#f79d14',
+  primaryBackgroundColor: '#15181a',
+  secondaryBackgroundColor: '#313031',
+  tertiaryBackgroundColor: '#000000',
+};
