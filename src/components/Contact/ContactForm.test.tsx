7a29c0e6a267208039d7938284bd1621d17b9d15
Martin Johnsson <maartinjohnsson@gmail.com>
2023-10-20 16:07:42 +0200

    Add should render without errors test to ContactForm.test

--- src/components/Contact/ContactForm.test.tsx
+++ src/components/Contact/ContactForm.test.tsx
@@ -3,3 +3,16 @@ import { screen } from '@testing-library/react';
 
 import { render } from 'src/utils/test-utils';
 import ContactForm from './ContactForm';
+
+describe('Contact Form', () => {
+  test('should render without errors', async () => {
+    //Arrange
+    render(<ContactForm />);
+
+    const introduction = screen.getByText('Want to get in contact?');
+
+    // Assert
+
+    expect(introduction).toBeInTheDocument();
+  });
+});
