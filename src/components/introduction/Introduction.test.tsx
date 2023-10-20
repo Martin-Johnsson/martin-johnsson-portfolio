04ff554074ec13bb6b3d51088d3f7616863b6232
Martin Johnsson <maartinjohnsson@gmail.com>
2023-10-20 11:28:44 +0200

    Add test for rendering Introduction without errors in Introduction.test.tsx

--- src/components/introduction/Introduction.test.tsx
+++ src/components/introduction/Introduction.test.tsx
@@ -2,3 +2,19 @@ import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
 
 import { render } from 'src/utils/test-utils';
+import Introduction from './Introduction';
+
+describe('Introduction', () => {
+  test('should render without errors', async () => {
+    //Arrange
+    render(<Introduction />);
+
+    const introduction = screen.getByText('Hi!');
+
+    // Act
+
+    // Assert
+
+    expect(introduction).toBeInTheDocument();
+  });
+});
