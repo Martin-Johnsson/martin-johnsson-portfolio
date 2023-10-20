6f667d4e1b844dc38c2f331b426819c8af6bc33f
Martin Johnsson <maartinjohnsson@gmail.com>
2023-10-20 11:51:01 +0200

    Add test in Introduction.test.tsx for pressing 'Let's get in touch' heading in Introduction

--- src/components/introduction/Introduction.test.tsx
+++ src/components/introduction/Introduction.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { render } from 'src/utils/test-utils';
 import Introduction from './Introduction';
@@ -16,5 +17,20 @@ describe('Introduction', () => {
     // Assert
 
     expect(introduction).toBeInTheDocument();
+  });
+
+  test(`should send user to Contact upon pressing "Let's get in touch`, async () => {
+    //Arrange
+    render(<Introduction />);
+
+    const letsGetInTouchHeading = screen.getByText(`Let's get in touch!`);
+
+    const contactHeadingText = screen.findByText('Want to get in contact?');
+
+    // Act
+    await userEvent.click(letsGetInTouchHeading);
+
+    // Assert
+    expect(contactHeadingText);
   });
 });
