6759a2eeae487dc4ae18d5aaee699580f32f070b
Martin Johnsson <maartinjohnsson@gmail.com>
2023-10-31 22:53:15 +0100

    Remove import of non existant file of index.css in pages/Projects.tsx

--- src/routes/pages/Projects.tsx
+++ src/routes/pages/Projects.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 
 import PreviousProjects from 'src/components/previousProjects/PreviousProjects';
-import 'src/styles/index.css';
 
 const Projects: FC = () => {
   return (
