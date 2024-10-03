0e8c28b7a896331c3d38292d929d980fa303962e
Martin Johnsson <maartinjohnsson@gmail.com>
2024-10-03 22:29:27 +0200

    ADD connection to mongodb database

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import projectsRoutes from './routes/projectsRoutes';
 
 import HttpError from './models/http-error';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -13,6 +14,13 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   throw error;
 });
 
-app.listen(3000, () => {
-  console.log('server is running');
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.idy5j0i.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err: typeof HttpError) => {
+    console.error(err);
+  });
