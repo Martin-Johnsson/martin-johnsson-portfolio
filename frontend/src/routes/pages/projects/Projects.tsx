2b93509963a1846b3c4fbd78119a738cdf6ae9be
Martin Johnsson <maartinjohnsson@gmail.com>
2024-10-20 18:07:36 +0200

    ADD usage of .env for backend variable and use LoadingSpinner while waiting for api response in Projects.tsx

--- frontend/src/routes/pages/projects/Projects.tsx
+++ frontend/src/routes/pages/projects/Projects.tsx
@@ -1,31 +1,32 @@
 import { useEffect, useState } from 'react';
 
-import { useHttpClient } from 'hooks/Http-hook';
-import { IProject } from 'types/interfaces';
+import { useHttpClient } from 'shared/hooks/Http-hook';
+import { IProject } from 'shared/types/interfaces';
 import ProjectList from 'components/projects/project/ProjectList/ProjectList';
+import LoadingSpinner from 'shared/components/LoadingSpinner/LoadingSpinner';
 
 const Projects = () => {
   const [loadedProjects, setLoadedProjects] = useState<[] | IProject[]>([]);
-  const { sendRequest } = useHttpClient();
+  const { sendRequest, isLoading } = useHttpClient();
+
+  const BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL;
 
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const responseData = await sendRequest(
-          'http://localhost:3000/api/projects',
-          null
-        );
+        const responseData = await sendRequest(BACKEND_URL + '/projects', null);
         setLoadedProjects(responseData.projects);
       } catch (err: unknown) {
         console.error(err);
       }
     };
     fetchProjects();
-  }, [sendRequest]);
+  }, [sendRequest, BACKEND_URL]);
 
   return (
-    <section>
-      <ProjectList loadedProjects={loadedProjects} />
+    <section className='center'>
+      {isLoading && <LoadingSpinner asOverlay />}
+      {!isLoading && <ProjectList loadedProjects={loadedProjects} />}
     </section>
   );
 };
