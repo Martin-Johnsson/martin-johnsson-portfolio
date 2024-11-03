692208b9840f2d1feabb60cc1f2b5a9c40995b44
Martin Johnsson <maartinjohnsson@gmail.com>
2024-11-03 21:21:10 +0100

    Add Access Control headers

--- backend/index.ts
+++ backend/index.ts
@@ -7,6 +7,16 @@ import mongoose from 'mongoose';
 
 const app = express();
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 app.use('/api/projects', projectsRoutes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
