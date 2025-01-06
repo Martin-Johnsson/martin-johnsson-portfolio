b5f2036d9c7b2741a65c9a450556331df47ac94b
Martin Johnsson <maartinjohnsson@gmail.com>
2025-01-06 21:37:29 +0100

    UPDATE theme object to store initial values for light and dark themes

--- frontend/src/shared/styles/theme/theme.ts
+++ frontend/src/shared/styles/theme/theme.ts
@@ -1,8 +1,62 @@
-export const theme = {
-  textColor: '#f5f0f0',
-  primaryAccentColor: '#483ab0',
-  secondaryAccentColor: '#f79d14',
-  primaryBackgroundColor: '#15181a',
-  secondaryBackgroundColor: '#313031',
-  tertiaryBackgroundColor: '#000000',
-};
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'system',
+  },
+
+  colors: {
+    light: {
+      backgrounds: {
+        primary: '#ffffff',
+      },
+      primaryTextColor: '#141414',
+      primaryAccentColor: '#483ab0',
+      icons: {
+        themeToggle: '#0c2f577a',
+      },
+    },
+    dark: {
+      backgrounds: {
+        primary: 'linear(to-r,#04173c, #020925)',
+        secondary: '#0d132b',
+      },
+      primaryTextColor: '#F5F5F5',
+      secondaryTextColor: 'rgb(100 116 139)',
+      primaryBorderColor: '#034078',
+      primaryAccentColor: '#483ab0',
+      icons: {
+        themeToggle: '#d8d10a',
+      },
+    },
+    shared: {
+      buttons: {
+        confirmButton: {
+          backgroundColor: '#32de85c9',
+          borderColor: '#32de85c9',
+          color: '#f5f0f0',
+        },
+      },
+    },
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        bgGradient:
+          props.colorMode === 'dark'
+            ? theme.colors.dark.backgrounds.primary
+            : '',
+        background:
+          props.colorMode === 'light'
+            ? theme.colors.light.backgrounds.primary
+            : '',
+        color:
+          props.colorMode === 'light'
+            ? theme.colors.light.primaryTextColor
+            : theme.colors.dark.primaryTextColor,
+      },
+    }),
+  },
+});
+
+export default theme;
