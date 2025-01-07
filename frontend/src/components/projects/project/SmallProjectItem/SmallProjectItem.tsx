9546570b4a6ff28a227e590f0ce5eee5629b7f6c
Martin Johnsson <maartinjohnsson@gmail.com>
2025-01-07 19:53:40 +0100

    REDESIGN SmallProjectItem to v2

--- frontend/src/components/projects/project/SmallProjectItem/SmallProjectItem.tsx
+++ frontend/src/components/projects/project/SmallProjectItem/SmallProjectItem.tsx
@@ -2,23 +2,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from '@reduxjs/toolkit';
 import { RootState } from 'shared/redux/store';
 
-import { Box, Flex, ListItem, UnorderedList, WrapItem } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  ListItem,
+  UnorderedList,
+  useColorMode,
+  WrapItem,
+} from '@chakra-ui/react';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-import {
-  LazyLoadingImage,
-  ProjectBox,
-} from 'components/projects/project/StyledComponentsProjects';
-import {
-  SmallScreenBodyText,
-  SmallScreenSmallBodyText,
-} from 'shared/styles/GlobalStyledComponents';
 
+import { LazyLoadingImage } from 'components/projects/project/StyledComponentsProjects';
 import ProjectsModal from 'components/projects/modal/Modal';
 import { IProjectState, ITag } from 'shared/types/interfaces';
+import { MobileProjectBoxDescription } from 'shared/styles/GlobalStyledComponents';
 
 const SmallProjectItem = (props) => {
+  const { colorMode } = useColorMode();
   const dispatch: Dispatch = useDispatch();
-
   const projectsState: IProjectState = useSelector(
     (state: RootState) => state.projects
   );
@@ -33,11 +34,7 @@ const SmallProjectItem = (props) => {
   };
 
   return (
-    <ProjectBox
-      onClick={() => {
-        handleExpandProject(props.index);
-      }}
-    >
+    <Box w='80vw'>
       {projectsState.modalIsOpen && (
         <ProjectsModal
           loadedProjects={props.loadedProjects}
@@ -46,61 +43,63 @@ const SmallProjectItem = (props) => {
         />
       )}
 
-      <WrapItem key={props.id}>
+      <WrapItem
+        backgroundColor={`${colorMode}.backgrounds.secondary`}
+        borderRadius='15px'
+      >
         <Box
-          boxShadow='0.14vh 0.14vw 1.98vh black'
-          key={props.id}
-          cursor='pointer'
           onClick={() => {
             handleExpandProject(props.index);
           }}
-          w='90vw'
+          padding='2vh 4vw'
+          w='100%'
         >
-          <Flex direction='column' alignItems='center'>
-            <Box alignSelf='center'>
-              <Flex justifySelf='center'>
-                <SmallScreenBodyText>{props.name}</SmallScreenBodyText>
-              </Flex>
-              <SmallScreenSmallBodyText>
+          <Flex direction='column'>
+            <Flex justifyContent='flex-end'>
+              <Box>
+                <span>
+                  <i className='fa-solid fa-expand'></i>
+                </span>
+              </Box>
+            </Flex>
+            <Flex alignItems='center' gap='10vw'>
+              <Box>
+                <LazyLoadingImage
+                  src={props.mainImage?.imageUrl}
+                  alt={props.mainImage?.alt}
+                />
+              </Box>
+              <Box>
+                <h3>{props.name}</h3>
+              </Box>
+            </Flex>
+            <Box mt='2vh'>
+              <MobileProjectBoxDescription>
                 {props.description}
-              </SmallScreenSmallBodyText>
-              <Flex marginRight='1vw' flexWrap='wrap' marginTop='1.5vh'>
-                <UnorderedList>
-                  {props.tags.map((tag: ITag) => {
-                    return (
-                      <ListItem
-                        key={tag.id}
-                        max-w='5'
-                        border='0.2rem solid #0b3d54'
-                        color='#f5f0f0'
-                        marginLeft='2vw'
-                        marginTop='1vh'
-                        display='inline-block'
-                        padding='0.2vw'
-                      >
-                        {tag.name}
-                      </ListItem>
-                    );
-                  })}
-                </UnorderedList>
-              </Flex>
-            </Box>
-            <Box
-              justifyContent='center'
-              marginBottom='10vh'
-              marginTop='5vh'
-              borderRadius='1.5vw'
-            >
-              <LazyLoadingImage
-                src={props.mainImage?.imageUrl}
-                alt={props.mainImage?.alt}
-                width='50vw'
-              />
+              </MobileProjectBoxDescription>
+              <UnorderedList mt='1.5vh'>
+                {props.tags.map((tag: ITag) => {
+                  return (
+                    <ListItem
+                      key={tag.id}
+                      borderRadius='25px'
+                      color={`${colorMode}.accentureTextColor`}
+                      backgroundColor={`${colorMode}.backgrounds.accenture`}
+                      display='inline-block'
+                      fontSize='0.8em'
+                      marginRight='1vw'
+                      padding='0.12rem'
+                    >
+                      {tag.name}
+                    </ListItem>
+                  );
+                })}
+              </UnorderedList>
             </Box>
           </Flex>
         </Box>
       </WrapItem>
-    </ProjectBox>
+    </Box>
   );
 };
 
